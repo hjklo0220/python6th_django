9961e16006e62eba1fcf58a403da4d6500452d32
JunYoung_Kim <hjklo0220@gmail.com>
2023-06-26 17:16:15 +0900

    year, month, day, today 아카이브 페이지 생성

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,47 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, ArchiveIndexView, YearArchiveView, MonthArchiveView, \
+	DayArchiveView, TodayArchiveView
 
 from blog.models import Post
 
 
 # Create your views here.
 class PostLV(ListView):
-	model = Post
-	template_name = 'blog/post_all.html'
-	context_object_name = 'posts'
-	paginate_by = 2  # 두개씩 보여줌(전체 글중에)
+    model = Post
+    template_name = 'blog/post_all.html'
+    context_object_name = 'posts'
+    paginate_by = 2
 
 
 class PostDV(DetailView):
-	model = Post
+    model = Post
+
+
+class PostAV(ArchiveIndexView):
+    model = Post
+    date_field = 'modify_dt'
+
+
+class PostYAV(YearArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+    make_object_list = True
+    # month_format = '%b'
+
+
+class PostMAV(MonthArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+    month_format = '%m'
+
+
+class PostDAV(DayArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+    month_format = '%m'
+
+
+class PostTAV(TodayArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+    template_name = 'blog/post_archive_day.html'
