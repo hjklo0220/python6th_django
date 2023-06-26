6b54fbdc2e466d820a68ba3931128b227a66ab21
JunYoung_Kim <hjklo0220@gmail.com>
2023-06-26 11:14:55 +0900

    admin model, bookmark migration

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from bookmark.models import Bookmark
+
 # Register your models here.
+
+# 데코레이터 패턴을 사용 하지 않는 방법
+# admin.site.register(Bookmark, BookmarkAdmin)
+
+
+@admin.register(Bookmark)
+class BookAdmin(admin.ModelAdmin):
+	list_display = ('id', 'title', 'url')
