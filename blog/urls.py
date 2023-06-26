7bbcedf68d271aa117decd90a342e4b5d08f6af3
JunYoung_Kim <hjklo0220@gmail.com>
2023-06-26 14:30:39 +0900

    index, detail 페이지 생성

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, re_path
+
+from blog import views
+
+app_name = 'blog'
+urlpatterns = [
+	path('', views.PostLV.as_view(), name='index'),
+	re_path(r'^post/(?P<slug>[-\w]+)/$', views.PostDV.as_view(), name='post_detail'),  # 정규표현식
+]
