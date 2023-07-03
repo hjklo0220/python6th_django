4856b1fc7dd7a5567e7f33696d11fd57630ca694
JunYoung_Kim <hjklo0220@gmail.com>
2023-07-03 10:42:50 +0900

    유저권한 관련 모델 수정

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 from taggit.managers import TaggableManager
@@ -12,6 +13,7 @@ class Post(models.Model):
 	created_dt = models.DateTimeField('CREATE DATE', auto_now_add=True)
 	modify_dt = models.DateTimeField('MODIFY DATE', auto_now=True)
 	tags = TaggableManager(blank=True)
+	owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='OWNER', blank=True, null=True)
 
 	class Meta:  # 부연설명이 필요한 것들?
 		verbose_name = 'post'
@@ -31,5 +33,10 @@ class Post(models.Model):
 
 	def get_next(self):
 		return self.get_next_by_modify_dt()
+
+	def save(self, *args, **kwargs):
+		from django.utils.text import slugify
+		self.slug = slugify(self.title, allow_unicode=True)
+		super().save(*args, **kwargs)
 
 
