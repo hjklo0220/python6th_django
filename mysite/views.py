1aca81e434075f4d6b6f64ee433c51c30d952e36
JunYoung_Kim <hjklo0220@gmail.com>
2023-07-04 09:46:50 +0900

    blog, mysite 뷰로직 추가

--- mysite/views.py
+++ mysite/views.py
@@ -9,14 +9,23 @@ class HomeView(TemplateView):
 
 
 class UserCreateView(CreateView):
-    template_name = 'registration/register.html'
-    form_class = UserCreationForm
-    success_url = reverse_lazy('register_done')
+	template_name = 'registration/register.html'
+	form_class = UserCreationForm
+	success_url = reverse_lazy('register_done')
 
 
 class UserCreateDoneTV(TemplateView):
-    template_name = 'registration/register_done.html'
+	template_name = 'registration/register_done.html'
 
 
 class OwnerOnlyMixin(AccessMixin):
-	pass
+	raise_exception = True
+	permission_denied_message = "Owner only can update/delete the object"
+
+	def dispatch(self, request, *args, **kwargs):
+		obj = self.get_object()
+		if request.user != obj.owner:
+			return self.handle_no_permission()
+		return super().dispatch(request, *args, **kwargs)
+
+
